c9397f93490a5aade72520cb59ee20a0a20ca110
Uvesh <uveshrajwani786@gmail.com>
2023-03-13 18:49:33 +0530

    Json with golang

--- 22 Json With Go/main.go
+++ 22 Json With Go/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 type course struct {
@@ -17,6 +19,7 @@ type course struct {
 func main() {
 	fmt.Println("Welcome to json with golang")
 	EncodeJson()
+	DecodeJson()
 }
 func EncodeJson() {
 	lcoCourse := []course{
@@ -31,4 +34,34 @@ func checkNilErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+func DecodeJson() {
+	const myUrl = "https://api.namaz.co.in"
+	myJsonData := []byte(`
+        {
+                "coursename": "Python",
+                "price": 0,
+                "platform": "Youtube",
+                "email": "py@go.dev",
+                "tags": [
+                        "py",
+                        "fastApi"
+                ]
+        }`)
+	response, _ := http.Get(myUrl)
+	data, err := io.ReadAll(response.Body)
+	checkNilErr(err)
+	defer response.Body.Close()
+	checkNilErr(err)
+	apiJsonValidCheck := json.Valid(data)
+	courseJsonValidCheck := json.Valid(myJsonData)
+	if apiJsonValidCheck && courseJsonValidCheck {
+		fmt.Println("All Json Is Valid")
+		fmt.Println("Api Json Data is \n", string(data))
+		var courseJsonData course
+		err := json.Unmarshal(myJsonData, &courseJsonData)
+		checkNilErr(err)
+		fmt.Println("Course Json Data is \n", courseJsonData)
+	}
+
 }
