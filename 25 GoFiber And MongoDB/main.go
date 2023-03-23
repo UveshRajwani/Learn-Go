fe4da5b163e4f97c76521edc11ed1710e871c3d4
Uvesh <uveshrajwani786@gmail.com>
2023-03-23 12:03:00 +0530

    get congif and get masjid

--- 25 GoFiber And MongoDB/main.go
+++ 25 GoFiber And MongoDB/main.go
@@ -14,12 +14,42 @@ import (
 	"time"
 )
 
+var objectIDFromHex = func(hex string) primitive.ObjectID {
+	objectID, err := primitive.ObjectIDFromHex(hex)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return objectID
+}
+
 type singeNamazModel struct {
-	Id    primitive.ObjectID `bson:"_id"`
+	Id    primitive.ObjectID `bson:"_id" json:"_id"`
 	Name  string             `json:"name"`
 	Start time.Time          `json:"start"`
 	End   time.Time          `json:"end"`
 	City  string             `json:"city"`
+}
+type singeMasjidModel struct {
+	Id            string `bson:"_id" json:"_id"`
+	Name          string `json:"name"`
+	MaplLnk       string `json:"map_link" bson:"map_link"`
+	Img           string `json:"img"`
+	BackgroundImg string `json:"background_img" bson:"background_img"`
+	City          string `json:"city"`
+	Area          string `json:"area"`
+	Timing        map[string]struct {
+		AzanTime   time.Time `json:"azan_time" bson:"azan_time"`
+		JammatTime time.Time `json:"jammat_time" bson:"jammat_time"`
+	} `json:"timing"`
+}
+type singeConfigModel struct {
+	Id         primitive.ObjectID `bson:"_id" json:"_id"`
+	City       string             `json:"city"`
+	IsRamzan   bool               `json:"isRamzan"`
+	IsLocation bool               `json:"isLocation"`
+	IsEid      bool               `json:"isEid"`
+	ChangeDate int                `json:"changeDate"`
+	TimeTable  string             `json:"timeTable"`
 }
 
 func main() {
@@ -32,10 +62,13 @@ func main() {
 	)
 	app.Use(logger.New())
 	app.Get("/getNamaz/:city/:namaz", getNamaz)
+	app.Get("/getMasjid/:id/", getMasjid)
+	app.Get("/getConfig/:city/", getConfig)
 	err := app.Listen(":3000")
 	checkNilErr(err)
 	//getNamaz()
 }
+
 func checkNilErr(err error) {
 	if err != nil {
 		panic(err)
@@ -53,8 +86,35 @@ func getNamaz(c *fiber.Ctx) error {
 	}
 	checkNilErr(err)
 	return c.JSON(namaz)
-
 }
+
+func getConfig(c *fiber.Ctx) error {
+	namazHandler, err := getMongoDbCollection("Namazdb", "config")
+	var config singeConfigModel
+	err = namazHandler.FindOne(context.TODO(), bson.D{
+		{"city", c.Params("city")},
+	}).Decode(&config)
+	if err == mongo.ErrNoDocuments {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"Error": "Config Not Found"})
+	}
+	checkNilErr(err)
+	return c.JSON(config)
+}
+func getMasjid(c *fiber.Ctx) error {
+
+	namazHandler, err := getMongoDbCollection("Namazdb", "masjids")
+	var masjid singeMasjidModel
+
+	err = namazHandler.FindOne(context.TODO(), bson.D{
+		{"_id", objectIDFromHex(c.Params("id"))},
+	}).Decode(&masjid)
+	if err == mongo.ErrNoDocuments {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"Error": "Masjid Not Found"})
+	}
+	checkNilErr(err)
+	return c.JSON(masjid)
+}
+
 func GetMongoDbConnection() (*mongo.Client, error) {
 
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://uvesh:uvesh786@167.86.86.55:27017"))
